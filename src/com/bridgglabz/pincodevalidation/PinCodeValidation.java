5633697e8ae1b39d4279637b55756006f08ecd72
Vinay288 <vinayhiremath288@gmail.com>
2021-09-08 01:47:53 +0530

    ADDED: logic to allow pincode with space in the middle

--- src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
+++ src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
@@ -4,30 +4,31 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PinCodeValidation {
+
 	public static boolean pincodeValidation(String pincode) {
-		String pincodeRegex = "^[0-9]{6}$";
+		String pincodeRegex = "^[0-9]{3}\\s{0,1}[0-9]{3}$";
 		return Pattern.matches(pincodeRegex, pincode);
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter pincode");
-		String pincode=scanner.nextLine();
-		while(!pincodeValidation(pincode)) {
+		String pincode = scanner.nextLine();
+		while (!pincodeValidation(pincode)) {
 			System.out.println("Invalid pincode.Enter again");
-			pincode=scanner.nextLine();
+			pincode = scanner.nextLine();
 		}
 		System.out.println("Valid pincode");
 		String checkPincode = "A400088";
-		System.out.println("Pincode to be checked:"+checkPincode);
-		if(pincodeValidation(checkPincode))
+		System.out.println("Pincode to be checked:" + checkPincode);
+		if (pincodeValidation(checkPincode))
 			System.out.println("Valid");
 		else
 			System.out.println("Invalid");
-		
+
 		checkPincode = "400088B";
-		System.out.println("Pincode to be checked:"+checkPincode);
-		if(pincodeValidation(checkPincode))
+		System.out.println("Pincode to be checked:" + checkPincode);
+		if (pincodeValidation(checkPincode))
 			System.out.println("Valid");
 		else
 			System.out.println("Invalid");
