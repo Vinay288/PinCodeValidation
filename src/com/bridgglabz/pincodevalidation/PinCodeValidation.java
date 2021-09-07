11485b02173e9893c284c5575470c2151efbca0c
Vinay288 <vinayhiremath288@gmail.com>
2021-09-08 01:45:28 +0530

    ADDED: check if regex allows alphabets

--- src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
+++ src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
@@ -5,19 +5,27 @@ import java.util.regex.Pattern;
 
 public class PinCodeValidation {
 
+
 	public static boolean pincodeValidation(String pincode) {
 		String pincodeRegex = "^[0-9]{6}$";
 		return Pattern.matches(pincodeRegex, pincode);
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter pincode");
-		String pincode = scanner.nextLine();
-		while (!pincodeValidation(pincode)) {
+		String pincode=scanner.nextLine();
+		while(!pincodeValidation(pincode)) {
 			System.out.println("Invalid pincode.Enter again");
-			pincode = scanner.nextLine();
+			pincode=scanner.nextLine();
 		}
+		System.out.println("Valid pincode");
+		String checkPincode = "A400088";
+		System.out.println("Pincode to be checked:"+checkPincode);
+		if(pincodeValidation(checkPincode))
+			System.out.println("Valid");
+		else
+			System.out.println("Invalid");
 
 	}
 }
