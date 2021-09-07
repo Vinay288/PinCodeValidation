5db49d40ff3957b9a54a83d5aa13bbd2b73be409
Vinay288 <vinayhiremath288@gmail.com>
2021-09-08 01:43:09 +0530

    ADDED: logic to accept 6 digit pincode

--- src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
+++ src/com/bridgglabz/pincodevalidation/PinCodeValidation.java
@@ -1,10 +1,23 @@
 package com.bridgglabz.pincodevalidation;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class PinCodeValidation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static boolean pincodeValidation(String pincode) {
+		String pincodeRegex = "^[0-9]{6}$";
+		return Pattern.matches(pincodeRegex, pincode);
 	}
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter pincode");
+		String pincode = scanner.nextLine();
+		while (!pincodeValidation(pincode)) {
+			System.out.println("Invalid pincode.Enter again");
+			pincode = scanner.nextLine();
+		}
+
+	}
 }
